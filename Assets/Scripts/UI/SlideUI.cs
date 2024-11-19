dfb441e4c7c2e5576abf2fe7bf6291668184a143
CaroPohn <carolinapohn@gmail.com>
2024-11-19 16:41:32 -0300

    Add credits animation

--- Assets/Scripts/UI/SlideUI.cs
+++ Assets/Scripts/UI/SlideUI.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlideUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Button _nextButton;
+    [SerializeField] private Button _backButton;
+
+    private bool _isInFirst = true;
+
+
+    private void Start()
     {
-        
+        _nextButton.onClick.AddListener(NextAnimation);
+        _backButton.onClick.AddListener(BackAnimation);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void NextAnimation()
     {
-        
+        if(!_isInFirst)
+        _animator.SetTrigger("Next");
+        _isInFirst = true;
+    }
+
+    private void BackAnimation()
+    {
+        if(_isInFirst)
+        _animator.SetTrigger("Back");
+        _isInFirst = false;
     }
 }
