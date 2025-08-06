24fcfb1a8657e53042acc6bfd3fbbabe895ea697
MateoMonastra <mateomonastravm@gmail.com>
2025-08-05 22:57:29 -0300

    Update ChainGhost animation

--- Assets/Scripts/Game/Ghosts/ChainGhost/ChainGhostAnimation.cs
+++ Assets/Scripts/Game/Ghosts/ChainGhost/ChainGhostAnimation.cs
@@ -4,15 +4,15 @@ namespace Game.Ghosts.ChainGhost
 {
     public class ChainGhostAnimation : MonoBehaviour
     {
-        private static readonly int Struggled = Animator.StringToHash("Struggled");
+        private static readonly int IsStruggled = Animator.StringToHash("isStruggled");
         private static readonly int Walk = Animator.StringToHash("Walk");
         private static readonly int IsTired = Animator.StringToHash("isTired");
         
         [SerializeField] private Animator animator;
 
-        public void SetStruggledAnimation()
+        public void SetStruggledAnimation(bool isStruggled)
         {
-            animator.SetTrigger(Struggled);
+            animator.SetBool(IsStruggled,isStruggled);
         }
 
         public void SetTiredAnimation(bool isTired)
